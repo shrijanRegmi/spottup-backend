c7705a1ba63c9746064cec8eb5e01eb97a564264
Shrijan <ilyyhs9@gmail.com>
2020-09-19 20:15:23 +0545

    accept decline notif sent only if is_accepted and is_declined value has changed

--- functions/lib/notifications/booking_accept_decline_notif.js
+++ functions/lib/notifications/booking_accept_decline_notif.js
@@ -5,65 +5,70 @@ const helper_1 = require("./helper");
 const bookingAcceptDeclineNotif = functions.firestore
     .document("bookings/{bookingId}")
     .onUpdate(async (snap) => {
+    const isAcceptedBefore = snap.before.data().is_accepted;
+    const isDeclinedBefore = snap.before.data().is_declined;
     const { is_accepted, is_declined, user_ref, hotel_ref, id, } = snap.after.data();
-    const hotelSnap = await hotel_ref.get();
-    const userSnap = await user_ref.get();
-    if (hotelSnap.exists && userSnap.exists) {
-        const hotelData = hotelSnap.data();
-        const userData = userSnap.data();
-        if (typeof hotelData !== "undefined" && typeof userData !== "undefined") {
-            console.log(`User ${userData.first_name} and hotel ${hotelData.name} found`);
-            if (is_accepted && !is_declined) {
-                console.log("Booking is accepted!");
-                const fcm = helper_1.default.prepareAcceptDeclineNotif(hotelData, id, true);
-                const notifTokens = await helper_1.default.getNotifTokens(user_ref);
-                await helper_1.default.sendNotification(notifTokens, fcm);
-                // add notification to notification collection
-                const notifData = {
-                    hotel_data: {
-                        id: hotelData.id,
-                        name: hotelData.name,
-                        dp: hotelData.dp,
-                    },
-                    user_data: {
-                        uid: userData.uid,
-                        first_name: userData.first_name,
-                        last_name: userData.last_name,
-                    },
-                    type: 0,
-                    is_read: false,
-                    booking_id: snap.after.id,
-                    hotel_ref: hotel_ref,
-                    last_updated: Date.now(),
-                };
-                await helper_1.default.addNotifToCollection(user_ref, notifData, hotelData.owner_id, userData.uid);
-            }
-            else {
-                console.log("Booking is declined");
-                const fcm = helper_1.default.prepareAcceptDeclineNotif(hotelData, id, false);
-                const notifTokens = await helper_1.default.getNotifTokens(user_ref);
-                await helper_1.default.sendNotification(notifTokens, fcm);
-                // add notification to notification collection
-                const notifData = {
-                    hotel_data: {
-                        id: hotelData.id,
-                        name: hotelData.name,
-                        dp: hotelData.dp,
-                    },
-                    user_data: {
-                        uid: userData.uid,
-                        first_name: userData.first_name,
-                        last_name: userData.last_name,
-                        photo_url: userData.photo_url,
-                    },
-                    type: 1,
-                    is_read: false,
-                    user_ref: user_ref,
-                    hotel_ref: hotel_ref,
-                    booking_id: snap.after.id,
-                    last_updated: Date.now(),
-                };
-                await helper_1.default.addNotifToCollection(user_ref, notifData, hotelData.owner_id, userData.uid);
+    if (isAcceptedBefore !== is_accepted && isDeclinedBefore !== is_declined) {
+        const hotelSnap = await hotel_ref.get();
+        const userSnap = await user_ref.get();
+        if (hotelSnap.exists && userSnap.exists) {
+            const hotelData = hotelSnap.data();
+            const userData = userSnap.data();
+            if (typeof hotelData !== "undefined" &&
+                typeof userData !== "undefined") {
+                console.log(`User ${userData.first_name} and hotel ${hotelData.name} found`);
+                if (is_accepted && !is_declined) {
+                    console.log("Booking is accepted!");
+                    const fcm = helper_1.default.prepareAcceptDeclineNotif(hotelData, id, true);
+                    const notifTokens = await helper_1.default.getNotifTokens(user_ref);
+                    await helper_1.default.sendNotification(notifTokens, fcm);
+                    // add notification to notification collection
+                    const notifData = {
+                        hotel_data: {
+                            id: hotelData.id,
+                            name: hotelData.name,
+                            dp: hotelData.dp,
+                        },
+                        user_data: {
+                            uid: userData.uid,
+                            first_name: userData.first_name,
+                            last_name: userData.last_name,
+                        },
+                        type: 0,
+                        is_read: false,
+                        booking_id: snap.after.id,
+                        hotel_ref: hotel_ref,
+                        last_updated: Date.now(),
+                    };
+                    await helper_1.default.addNotifToCollection(user_ref, notifData, hotelData.owner_id, userData.uid);
+                }
+                else {
+                    console.log("Booking is declined");
+                    const fcm = helper_1.default.prepareAcceptDeclineNotif(hotelData, id, false);
+                    const notifTokens = await helper_1.default.getNotifTokens(user_ref);
+                    await helper_1.default.sendNotification(notifTokens, fcm);
+                    // add notification to notification collection
+                    const notifData = {
+                        hotel_data: {
+                            id: hotelData.id,
+                            name: hotelData.name,
+                            dp: hotelData.dp,
+                        },
+                        user_data: {
+                            uid: userData.uid,
+                            first_name: userData.first_name,
+                            last_name: userData.last_name,
+                            photo_url: userData.photo_url,
+                        },
+                        type: 1,
+                        is_read: false,
+                        user_ref: user_ref,
+                        hotel_ref: hotel_ref,
+                        booking_id: snap.after.id,
+                        last_updated: Date.now(),
+                    };
+                    await helper_1.default.addNotifToCollection(user_ref, notifData, hotelData.owner_id, userData.uid);
+                }
             }
         }
     }
--- functions/src/notifications/booking_accept_decline_notif.ts
+++ functions/src/notifications/booking_accept_decline_notif.ts
@@ -4,6 +4,9 @@ import notifHelper from "./helper";
 const bookingAcceptDeclineNotif = functions.firestore
   .document("bookings/{bookingId}")
   .onUpdate(async (snap) => {
+    const isAcceptedBefore = snap.before.data().is_accepted;
+    const isDeclinedBefore = snap.before.data().is_declined;
+
     const {
       is_accepted,
       is_declined,
@@ -12,96 +15,101 @@ const bookingAcceptDeclineNotif = functions.firestore
       id,
     } = snap.after.data();
 
-    const hotelSnap = await hotel_ref.get();
-    const userSnap = await user_ref.get();
+    if (isAcceptedBefore !== is_accepted && isDeclinedBefore !== is_declined) {
+      const hotelSnap = await hotel_ref.get();
+      const userSnap = await user_ref.get();
 
-    if (hotelSnap.exists && userSnap.exists) {
-      const hotelData = hotelSnap.data();
-      const userData = userSnap.data();
+      if (hotelSnap.exists && userSnap.exists) {
+        const hotelData = hotelSnap.data();
+        const userData = userSnap.data();
 
-      if (typeof hotelData !== "undefined" && typeof userData !== "undefined") {
-        console.log(
-          `User ${userData.first_name} and hotel ${hotelData.name} found`
-        );
-
-        if (is_accepted && !is_declined) {
-          console.log("Booking is accepted!");
-
-          const fcm = notifHelper.prepareAcceptDeclineNotif(
-            hotelData,
-            id,
-            true
+        if (
+          typeof hotelData !== "undefined" &&
+          typeof userData !== "undefined"
+        ) {
+          console.log(
+            `User ${userData.first_name} and hotel ${hotelData.name} found`
           );
 
-          const notifTokens = await notifHelper.getNotifTokens(user_ref);
+          if (is_accepted && !is_declined) {
+            console.log("Booking is accepted!");
 
-          await notifHelper.sendNotification(notifTokens, fcm);
+            const fcm = notifHelper.prepareAcceptDeclineNotif(
+              hotelData,
+              id,
+              true
+            );
 
-          // add notification to notification collection
-          const notifData = {
-            hotel_data: {
-              id: hotelData.id,
-              name: hotelData.name,
-              dp: hotelData.dp,
-            },
-            user_data: {
-              uid: userData.uid,
-              first_name: userData.first_name,
-              last_name: userData.last_name,
-            },
-            type: 0,
-            is_read: false,
-            booking_id: snap.after.id,
-            hotel_ref: hotel_ref,
-            last_updated: Date.now(),
-          };
+            const notifTokens = await notifHelper.getNotifTokens(user_ref);
 
-          await notifHelper.addNotifToCollection(
-            user_ref,
-            notifData,
-            hotelData.owner_id,
-            userData.uid
-          );
-        } else {
-          console.log("Booking is declined");
+            await notifHelper.sendNotification(notifTokens, fcm);
 
-          const fcm = notifHelper.prepareAcceptDeclineNotif(
-            hotelData,
-            id,
-            false
-          );
+            // add notification to notification collection
+            const notifData = {
+              hotel_data: {
+                id: hotelData.id,
+                name: hotelData.name,
+                dp: hotelData.dp,
+              },
+              user_data: {
+                uid: userData.uid,
+                first_name: userData.first_name,
+                last_name: userData.last_name,
+              },
+              type: 0,
+              is_read: false,
+              booking_id: snap.after.id,
+              hotel_ref: hotel_ref,
+              last_updated: Date.now(),
+            };
 
-          const notifTokens = await notifHelper.getNotifTokens(user_ref);
+            await notifHelper.addNotifToCollection(
+              user_ref,
+              notifData,
+              hotelData.owner_id,
+              userData.uid
+            );
+          } else {
+            console.log("Booking is declined");
 
-          await notifHelper.sendNotification(notifTokens, fcm);
+            const fcm = notifHelper.prepareAcceptDeclineNotif(
+              hotelData,
+              id,
+              false
+            );
 
-          // add notification to notification collection
-          const notifData = {
-            hotel_data: {
-              id: hotelData.id,
-              name: hotelData.name,
-              dp: hotelData.dp,
-            },
-            user_data: {
-              uid: userData.uid,
-              first_name: userData.first_name,
-              last_name: userData.last_name,
-              photo_url: userData.photo_url,
-            },
-            type: 1,
-            is_read: false,
-            user_ref: user_ref,
-            hotel_ref: hotel_ref,
-            booking_id: snap.after.id,
-            last_updated: Date.now(),
-          };
+            const notifTokens = await notifHelper.getNotifTokens(user_ref);
 
-          await notifHelper.addNotifToCollection(
-            user_ref,
-            notifData,
-            hotelData.owner_id,
-            userData.uid
-          );
+            await notifHelper.sendNotification(notifTokens, fcm);
+
+            // add notification to notification collection
+            const notifData = {
+              hotel_data: {
+                id: hotelData.id,
+                name: hotelData.name,
+                dp: hotelData.dp,
+              },
+              user_data: {
+                uid: userData.uid,
+                first_name: userData.first_name,
+                last_name: userData.last_name,
+                photo_url: userData.photo_url,
+              },
+              type: 1,
+              is_read: false,
+              user_ref: user_ref,
+              hotel_ref: hotel_ref,
+              booking_id: snap.after.id,
+              last_updated: Date.now(),
+            };
+
+            await notifHelper.addNotifToCollection(
+              user_ref,
+              notifData,
+              hotelData.owner_id,
+              userData.uid
+            );
+          }
         }
       }
     }
